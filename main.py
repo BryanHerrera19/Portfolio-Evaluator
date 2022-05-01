1946e938e03e26cf3fd1b05f44a0263e472160d2
Bryan Herrera <72991369+BryanHerrera19@users.noreply.github.com>
2022-04-30 19:51:02 -0700

    Update main.py
    Updated with final version of the code which implements evaluation of blocks of code for small imperative language.

--- main.py
+++ main.py
@@ -1,17 +1,182 @@
 #Bryan Herrera, Bryant Tran
 #COMP_141 Group 15
-#Project Phase 2.1
-
-# expression ::=term { + term }
-# term ::=factor { - factor }
-# factor ::=piece { / piece }
-# piece ::=element { * element }
-# element ::=( expression ) | NUMBER | IDENTIFIER
+#Project Phase 3.1
+#Evaluator for expressions
 
 import sys
 import re
 
 listCounter = 0
+memory = {}
+  
+  
+def parseStatement(tokenList):
+  global listCounter
+  
+  tree = parseBaseStatement(tokenList)
+
+  if(listCounter == len(tokenList)):
+    return tree
+  
+  while(tokenList[listCounter].tokenValue == ";"):
+    listCounter += 1
+    tree = Node(tokenList[listCounter - 1], tree, None , parseBaseStatement(tokenList))
+    if(listCounter == len(tokenList)):
+      break
+  return tree  
+
+#---------------------------------------------------------------#
+
+def parseBaseStatement(tokenList):
+  global listCounter
+  
+  if(tokenList[listCounter].tokenValue == "if"):
+    return parseIfStatement(tokenList)
+    
+  elif(tokenList[listCounter].tokenValue == "while"):
+    return parseWhileStatement(tokenList)
+    
+  elif(tokenList[listCounter].tokenValue == "skip"):
+    listCounter += 1
+    tree = Node(tokenList[listCounter - 1], None, None , None)
+    return tree
+    
+  elif(tokenList[listCounter].tokenType == "IDENTIFIER"):
+    tree = parseAssignment(tokenList)
+    return tree
+    
+  else:
+    print("ERROR 1! " + str(tokenList[listCounter].tokenValue) +  " Does Not Match Grammar!\n")
+    quit()
+    
+#---------------------------------------------------------------#
+
+def parseAssignment(tokenList):
+  global listCounter
+  
+  tree = parseElement(tokenList)
+    
+  if(tokenList[listCounter].tokenValue == ":="):
+    currentToken = listCounter
+    listCounter += 1
+    expression = parseExpression(tokenList)
+    tree = Node(tokenList[currentToken], tree, None, expression)
+    return tree
+
+#---------------------------------------------------------------#
+
+def parseIfStatement(tokenList):
+  global listCounter
+  
+  currentToken = listCounter
+  listCounter += 1
+  tree = parseExpression(tokenList)
+  
+  if(tokenList[listCounter].tokenValue == "then"):
+    listCounter += 1
+    tree2 = parseStatement(tokenList)
+
+    if(tokenList[listCounter].tokenValue == "else"):
+      listCounter += 1
+      tree3 = parseStatement(tokenList)
+
+      if(tokenList[listCounter].tokenValue == "endif"):
+        listCounter += 1
+        return Node(tokenList[currentToken], tree, tree2, tree3)
+        
+  else:
+    print("ERROR 2! " + str(tokenList[listCounter].tokenValue) +  " Does Not Match Grammar!\n")
+    quit()
+    
+#---------------------------------------------------------------#
+
+def parseWhileStatement(tokenList):
+  global listCounter
+  
+  currentToken = listCounter
+  listCounter += 1
+  tree = parseExpression(tokenList)
+  
+  if(tokenList[listCounter].tokenValue == "do"):
+    listCounter += 1
+    tree2 = parseStatement(tokenList)
+
+    if(tokenList[listCounter].tokenValue == "endwhile"):
+      listCounter += 1
+    return Node(tokenList[currentToken], tree, None, tree2)
+    
+  else:
+    print("ERROR 3! " + str(tokenList[listCounter].tokenValue) +  " Does Not Match Grammar!\n")
+    quit()
+
+  
+#---------------------------------------------------------------#
+
+def isLeaf (node):
+  return node.left is None and node.right is None
+
+#---------------------------------------------------------------#
+
+def evaluateAssignment(node):
+  key = node.left.data.tokenValue
+  value = evaluateExpression(node.right)
+  memory[key] = value
+  
+def evaluateStatement(node):
+  if node.data.tokenValue == ";":
+    evaluateStatement(node.left)
+    evaluateStatement(node.right)
+  elif node.data.tokenValue == ":=":
+    evaluateAssignment(node)
+  elif node.data.tokenValue == "if":
+    evaluateIf(node)
+  elif node.data.tokenValue == "while":
+    evaluateWhile(node)
+  elif node.data.tokenValue == "skip":
+    return
+  else:
+    print("Error statement")
+
+def evaluateWhile(node):
+  v = evaluateExpression(node.left)
+  while v != 0:
+    evaluateStatement(node.right)
+    v = evaluateExpression(node.left)
+  
+def evaluateIf(node):
+  if evaluateExpression(node.left) != 0:
+    evaluateStatement(node.middle)
+  else:
+    evaluateStatement(node.right)
+    
+  
+def evaluateExpression (node):
+  if node is None:
+    return 0
+  if node.data.tokenType == "IDENTIFIER":
+    if node.data.tokenValue not in memory:
+      print("Error missing parentheses")
+      quit()
+    return memory[node.data.tokenValue]
+  if node.data.tokenType == "NUMBER":
+    return int(node.data.tokenValue)    
+  left = evaluateExpression(node.left)
+  right = evaluateExpression(node.right)
+  return solve(node.data, left , right)
+
+#---------------------------------------------------------------#
+
+def solve(op, left , right):
+  if op.tokenValue == '+':
+    return left + right
+  if op.tokenValue == '-':
+    return max(0, left - right)
+  if op.tokenValue == '*':
+    return left * right
+  if op.tokenValue == '/':
+    return left // right
+  
+#---------------------------------------------------------------#
 
 def printTree(tree, n, file):
   if tree == None:
@@ -27,7 +192,6 @@ def parseExpression(tokenList):
   global listCounter
   tree = parseTerm(tokenList)
   while(tokenList[listCounter].tokenValue == "+"):
-    
     listCounter += 1
     tree = Node(tokenList[listCounter - 1], tree, None , parseTerm(tokenList))
   return tree
@@ -38,7 +202,6 @@ def parseTerm(tokenList):
   global listCounter
   tree = parseFactor(tokenList)
   while(tokenList[listCounter].tokenValue == "-"):
-    
     listCounter += 1
     tree = Node(tokenList[listCounter - 1], tree, None , parseFactor(tokenList))
   return tree
@@ -49,7 +212,6 @@ def parseFactor(tokenList):
   global listCounter
   tree = parsePiece(tokenList)
   while(tokenList[listCounter].tokenValue == "/"):
-    
     listCounter += 1
     tree = Node(tokenList[listCounter - 1], tree, None , parsePiece(tokenList))
   return tree
@@ -60,7 +222,6 @@ def parsePiece(tokenList):
   global listCounter
   tree = parseElement(tokenList)
   while(tokenList[listCounter].tokenValue == "*"):
-
     listCounter += 1
     tree = Node(tokenList[listCounter - 1], tree, None , parseElement(tokenList))
   return tree
@@ -72,16 +233,14 @@ def parseElement(tokenList):
     
   if tokenList[listCounter].tokenType == "NUMBER":
     tree = Node(tokenList[listCounter] , None , None, None)
-    listCounter += 1
-    if(listCounter == len(tokenList)):
-      listCounter -= 1
+    if(listCounter < len(tokenList) - 1):
+      listCounter += 1
     return tree
   
   if tokenList[listCounter].tokenType == "IDENTIFIER":
     tree = Node(tokenList[listCounter] , None , None, None)
-    listCounter += 1
-    if(listCounter == len(tokenList)):
-      listCounter -= 1
+    if(listCounter < len(tokenList) - 1):
+      listCounter += 1
     return tree
   
   if tokenList[listCounter].tokenValue == "(":
@@ -89,6 +248,8 @@ def parseElement(tokenList):
     listCounter += 1
     tree = parseExpression(tokenList)
     if tokenList[listCounter].tokenValue == ")":
+      if(listCounter < len(tokenList) - 1):
+        listCounter += 1
       return tree
     else :
       print("Error missing parentheses")
@@ -108,7 +269,8 @@ class Node:
 def split(word): #Function to split words into chars
   return [char for char in word]
 
-#---------------------------------------------------------------#  
+#---------------------------------------------------------------#
+ 
 class Token:
   def __init__(self , tokenType , tokenValue):
     self.tokenType = tokenType
@@ -123,7 +285,8 @@ def scanner(line):
   words = line.split()
   identifier = re.compile(r'^([a-z]|[A-Z])([a-z]|[A-Z]|[0-9])*')
   number = re.compile(r'^[0-9]+')
-  symbol = re.compile(r'\+|\-|\*|/|\(|\)')
+  symbol = re.compile(r'\+|\-|\*|/|\(|\)|:=|;')
+  keyword = re.compile(r'if|then|else|endif|while|do|endwhile|skip')
 
   tokens = []
   for word in words:
@@ -133,7 +296,10 @@ def scanner(line):
 
       for j in range (i, len(word)):
 
-        if identifier.fullmatch(word[i : j + 1]) != None:
+        if keyword.fullmatch(word[i : j + 1]) != None:
+          longesttoken = (Token("KEYWORD", word[i : j + 1]))
+
+        elif identifier.fullmatch(word[i : j + 1]) != None:
           longesttoken = (Token("IDENTIFIER" , word[i : j + 1]))
       
         elif number.fullmatch(word[i : j + 1]) != None:
@@ -142,7 +308,6 @@ def scanner(line):
         elif symbol.fullmatch(word[i : j + 1]) != None:
           longesttoken =(Token("SYMBOL" , word[i : j + 1]))
           
-        #tokens.append(Token("ERROR" , word))
       if longesttoken is None:
         tokens.append(Token("ERROR" , word[i]))
         return tokens
@@ -166,23 +331,32 @@ def main():
   
   fIn = open(fileIn, 'r')
   fOut = open(fileOut, 'w')
-  #with open(fileIn, 'r') as fIn, open(fileOut, 'w') as fOut:  #Opens input file to read
-  for line in fIn:  #Iterate thorugh input file line by line
-    line = line.strip()
-    print("Line: " + line)
-    fOut.write("Line: " + line + "\n")  #Writes current line to be scanned to output file
-    tokenList = scanner(line)
-      
-    for token in tokenList: #Iterates through tokens in current list
-      fullTokenList.append(token)
-      print(token) #prints token
-      fOut.write(token.tokenType + " : " + token.tokenValue + "\n") #Writes token to output file
-        
-  fOut.write("\nAST:\n")
-  print("\nAST:\n")
-  tree = parseExpression(fullTokenList)
-  printTree(tree, 0, fOut)
+  with open(fileIn, 'r') as fIn, open(fileOut, 'w') as fOut:  #Opens input file to read
+    for line in fIn:  #Iterate thorugh input file line by line
+      line = line.strip()
+      print("Line: " + line)
+      fOut.write("Line: " + line + "\n")  #Writes current line to be scanned to output file
+    
+      tokenList = scanner(line)  
+      for token in tokenList: #Iterates through tokens in current list
+        fullTokenList.append(token)
+        print(token) #prints token
+        fOut.write(token.tokenType + " : " + token.tokenValue + "\n") #Writes token to output file
 
+      fOut.write("\n")
+      print("\n")
+        
+    fOut.write("\nAST:\n")
+    print("\nAST:\n")
+    tree = parseStatement(fullTokenList)
+    printTree(tree, 0, fOut)
+    evaluateStatement(tree)
+    print("\nOutput: \n")
+    fOut.write("\nOutput\n")
+    
+    for key, value in memory.items():
+      print(str(key) + "=" + str(value) + "\n")
+      fOut.write(str(key) + "=" + str(value) + "\n")
   fIn.close()
   fOut.close()
 
